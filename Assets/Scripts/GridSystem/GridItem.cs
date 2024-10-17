c8e12d17c6c4ee5962ba3c4b33dd1c22f6ff65d5
Leon Higgin <admin@leonmichaelhiggin.co.uk>
2024-10-17 23:45:08 +0100

    Grid system

---
+++ Assets/Scripts/GridSystem/GridItem.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+namespace GridSystem
+{
+    /// <summary>
+    /// Items with 1 or 0 neighbors die due to solitude.
+    /// Items with 3 or more neighbors die due to over population.
+    /// Items with 2 or 3 neighbors survive.
+    /// items with 3 neighbors becomes populated
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    [RequireComponent(typeof(RawImage))]
+    [RequireComponent(typeof(Outline))]
+    public class GridItem : MonoBehaviour
+    {
+        public KeyValuePair<int, int> Indices { get;  private set; } //Key = X, Value = Y
+        private bool Initialised { get; set; }
+        public bool Populated { get; private set; }
+        private bool randomizeColour;
+        private Action<GridItem> onClick;
+        private Button button;
+        private RawImage display;
+        private Outline outline;
+        private GridItem[] neighbors;
+        private Color aliveColour;
+        private Color deadColour;
+
+        public void Initialise(KeyValuePair<int, int> indices, Color alive, Color dead, Action<GridItem> gridManagerOnClick)
+        {
+            if (Initialised)
+            {
+                Debug.LogError($"Do not initialise {nameof(GridItem)} more than once.");
+                return;
+            }
+
+            button = GetComponent<Button>(); //Secured by the require component attribute.
+            display = GetComponent<RawImage>();
+            outline = GetComponent<Outline>();
+            button.onClick.AddListener(OnClick);
+            onClick += gridManagerOnClick;
+            Indices = indices;
+            aliveColour = alive;
+            deadColour = dead;
+            Initialised = true;
+        }
+        
+        public void EnableInteractions(bool state = true)
+        {
+            button.enabled = state;
+        }
+        
+        public void EnableHighlight(bool state = true)
+        {
+            outline.enabled = state;
+        }
+        
+        public void EnableRandomisedColour(bool state = true)
+        {
+            randomizeColour = state;
+        }
+
+        public void SetNeighbors()
+        {
+            neighbors = new[]
+            {
+                //Row Above
+                GridManager.Instance.GetItem(new KeyValuePair<int, int>(Indices.Key-1,Indices.Value-1)),
+                GridManager.Instance.GetItem(new KeyValuePair<int, int>(Indices.Key-1,Indices.Value)),
+                GridManager.Instance.GetItem(new KeyValuePair<int, int>(Indices.Key-1,Indices.Value+1)),
+                //Same Row
+                GridManager.Instance.GetItem(new KeyValuePair<int, int>(Indices.Key,Indices.Value-1)),
+                GridManager.Instance.GetItem(new KeyValuePair<int, int>(Indices.Key,Indices.Value+1)),
+                //Row Below
+                GridManager.Instance.GetItem(new KeyValuePair<int, int>(Indices.Key+1,Indices.Value-1)),
+                GridManager.Instance.GetItem(new KeyValuePair<int, int>(Indices.Key+1,Indices.Value)),
+                GridManager.Instance.GetItem(new KeyValuePair<int, int>(Indices.Key+1,Indices.Value+1))
+            };
+            
+            neighbors = neighbors.Where(x => x != null).ToArray(); //Remove edge piece neighbors
+        }
+
+        private void OnDisable()
+        {
+            if (onClick != null)
+            {
+                onClick -= GridManager.OnItemClick;
+            }
+        }
+
+        private void OnClick()
+        {
+            if (!Initialised)
+            {
+                Debug.LogError($"{nameof(GridItem)}{Indices.Key}{Indices.Value} has not been initialised.");
+                return;
+            }
+            SetState(!Populated);
+            SetDisplay(Populated);
+            onClick(this);
+        }
+        
+        public void OnStateChange()
+        {
+            CheckPopulate();
+            CheckDeath();
+        }
+        
+        private void CheckPopulate()
+        {
+            if (Populated) return;
+            
+            if (!ShouldPopulate()) return;
+            
+            SetState(true);
+            SetDisplay(true);
+        }
+
+        private void CheckDeath()
+        {
+            if (!Populated) return;
+
+            if (!ShouldDie()) return;
+            
+            SetState(false);
+            SetDisplay(false);
+        }
+
+        private bool ShouldDie()
+        {
+            return NeighborCountGreaterThanThree() || NeighborCountLessThanTwo();
+        }
+        
+        private bool ShouldPopulate()
+        {
+            return NeighborCountEqualsThree();        
+        }
+
+        private void SetState(bool state)
+        {
+            Populated = state;
+        }
+        
+        private void SetDisplay(bool state)
+        {
+            display.color = GetColour(state);
+        }
+
+        private Color GetColour(bool state)
+        {
+            if (randomizeColour)
+            {
+                aliveColour = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), 1);
+            }
+            return state ? aliveColour : deadColour;
+        }
+
+        public void ResetItem(bool instant = true)
+        {
+            SetState(true);
+        }
+        
+        private bool NeighborCountGreaterThanThree()
+        {
+            var count = 0;
+            foreach (var neighbor in neighbors)
+            {
+                if (neighbor.Populated)
+                {
+                    count++;
+                }
+            }
+            return count > 3;
+        }
+        
+        private bool NeighborCountEqualsThree()
+        {
+            var count = 0;
+            foreach (var neighbor in neighbors)
+            {
+                if (neighbor.Populated)
+                {
+                    count++;
+                }
+            }
+            return count == 3;
+        }
+
+        private bool NeighborCountLessThanTwo()
+        {
+            var count = 0;
+            foreach (var neighbor in neighbors)
+            {
+                if (neighbor.Populated)
+                {
+                    count++;
+                }
+            }
+            return count < 2;
+        }
+    }
+}
---
+++ Assets/Scripts/GridSystem/GridManager.cs
@@ -0,0 +1,237 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using pure_unity_methods.Abstraction;
+using pure_unity_methods.StateManagement;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace GridSystem
+{
+    [ExecuteInEditMode]
+    public class GridManager : Singleton<GridManager>
+    {
+        public static Action<GridItem> OnItemClick;
+        private bool generatingGrid;
+        private const string GridTag = "Grid";
+        private readonly List<GridItem> gridItems = new ();
+        private GameObject gridObject;
+        [SerializeField] private Transform gridArea;
+        [Header("Prefabs")]
+        [SerializeField] private GameObject gridPrefab;
+        [SerializeField] private GameObject rowPrefab;
+        [SerializeField] private GameObject cardPrefab;
+        [Header("Colors")]
+        [SerializeField] private Color aliveColour = Color.black;
+        [SerializeField] private Color deadColour = Color.white;
+        [Header("Settings")]
+        private const int MinimumItems = 3;
+        private const int MaximumRows = 60;
+        private const int MaximumColumns = 30;
+        [SerializeField] [Range(MinimumItems, MaximumRows)] public int amountOfRows;
+        [SerializeField] [Range(MinimumItems, MaximumColumns)] public int amountOfColumns;
+        [SerializeField] public Slider rowsSlider;
+        [SerializeField] public Slider columnsSlider;
+
+        public IEnumerator Initialise(Action completeCallback)
+        {
+            SetSliders();
+            yield return new WaitUntil(() => !generatingGrid);
+            GenerateGrid(completeCallback);
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            SequentialStateManager.OnStateChange += OnStateChange;
+        }
+        
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            SequentialStateManager.OnStateChange -= OnStateChange;
+        }
+
+        private void SetSliders()
+        {
+            rowsSlider.wholeNumbers = false;
+            rowsSlider.value = amountOfRows;
+            rowsSlider.minValue = MinimumItems;
+            rowsSlider.maxValue = MaximumRows;
+            rowsSlider.onValueChanged.AddListener(OnRowsSliderValueChanged);
+            
+            rowsSlider.wholeNumbers = false;
+            columnsSlider.value = amountOfColumns;
+            columnsSlider.minValue = MinimumItems;
+            columnsSlider.maxValue = MaximumColumns;
+            columnsSlider.onValueChanged.AddListener(OnColumnsSliderValueChanged);
+        }
+
+        private void OnStateChange()
+        {
+            foreach (var item in gridItems)
+            {
+                item.OnStateChange();
+            }
+        }
+        
+        public void OnRowsSliderValueChanged(float f)
+        {
+            amountOfRows = (int)f;
+            GenerateGrid();
+        }
+        
+        public void OnColumnsSliderValueChanged(float f)
+        {
+            amountOfColumns = (int)f;
+            GenerateGrid();
+        }
+
+        private static void OnGridItemClick(GridItem gridItem)
+        {
+            //Debug.Log($"{gridItem.Value.GetRank()} of {gridItem.Value.GetSuit()} revealed({gridItem.Revealed})"); //Debug tool
+            OnItemClick?.Invoke(gridItem);
+        }
+        
+        private void SetNeighbors()
+        {
+            foreach (var item in gridItems)
+            {
+                item.SetNeighbors();
+            }
+        }
+        
+        public void OptimiseGrid()
+        {
+            EnableInteractions();
+        }
+        
+        public void EnableInteractions(bool state = true)
+        {
+            foreach (var item in gridItems)
+            {
+                item.EnableInteractions(state);
+            }
+        }
+
+        public void EnableHighlight(bool state = true)
+        {
+            foreach (var item in gridItems)
+            {
+                item.EnableHighlight(state);
+            }
+        }
+        
+        public void EnableRandomisedColour(bool state = true)
+        {
+            foreach (var item in gridItems)
+            {
+                item.EnableRandomisedColour(state);
+            }
+        }
+
+        public GridItem GetItem(KeyValuePair<int, int> indices)
+        {
+            var x = indices.Key; //Cache for LINQs hidden multiple access.
+            var y = indices.Value;
+            return gridItems.Where(gridItem => gridItem.Indices.Key == x).FirstOrDefault(gridItem => gridItem.Indices.Value == y);
+        }
+
+        public bool IsGameValid()
+        {
+            return gridItems.Any(x => x.Populated);
+        }
+
+        #region Generate Grid
+        #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (Application.isPlaying) return;
+            if (generatingGrid) return;
+            GenerateGrid();
+        }
+
+        [ContextMenu(nameof(ResetGridGenerationSystem))]
+        public void ResetGridGenerationSystem()
+        {
+            generatingGrid = false;
+            amountOfRows = MinimumItems;
+            amountOfColumns = MinimumItems;
+            GenerateGrid();
+        }
+        #endif
+        
+        private void GenerateGrid(Action completeCallback = null)
+        {
+            if (generatingGrid) return;
+            generatingGrid = true;
+            ResetGrid(()=>CreateGrid(() =>
+            {
+                gridObject = GameObject.FindWithTag(GridTag);
+                generatingGrid = false;
+                completeCallback?.Invoke();
+            }));
+        }
+
+        private void CreateGrid(Action completeCallback = null)
+        {
+            gridObject = Instantiate(gridPrefab, gridArea);
+            for (var i = 0; i < amountOfRows; i++)
+            {
+                var row = Instantiate(rowPrefab, gridObject.transform).GetComponent<GridRow>();
+                for (var j = 0; j < amountOfColumns; j++)
+                {
+                    if (Application.isPlaying)
+                    {
+                        var item = Instantiate(cardPrefab, row.transform).GetComponent<GridItem>();
+                        item.Initialise(new KeyValuePair<int, int>(j, i), aliveColour, deadColour, OnGridItemClick);
+                        gridItems.Add(item);
+                    }
+                    else
+                    {
+                        Instantiate(cardPrefab, row.transform);
+                    }
+                }
+            }
+            SetNeighbors();
+            completeCallback?.Invoke();
+        }
+
+        private void ResetGrid(Action completeCallback = null)
+        {
+            ResetGridItems();
+            gridObject = GameObject.FindWithTag(GridTag);
+            if (!gridObject)
+            {
+                completeCallback?.Invoke();
+                return;
+            }
+            if (Application.isPlaying)
+            {
+                Destroy(gridObject);
+                completeCallback?.Invoke();
+            }
+            else
+            {
+                UnityEditor.EditorApplication.delayCall+=()=>
+                {
+                    DestroyImmediate(gridObject);
+                    completeCallback?.Invoke();
+                };
+            }
+        }
+    
+        private void ResetGridItems()
+        {
+            foreach (var item in gridItems)
+            {
+                item.ResetItem();
+            }
+            gridItems.Clear();
+        }
+        #endregion Generate Grid
+    }
+}
+
